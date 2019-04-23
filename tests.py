9924ea3d2476245a259c6b2dab8b7127789e561b
Lindsay Barrett <labarrett22@gmail.com>
2019-04-22 17:07:36 -0700

    Added more date tests
    Added more date tests

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest import TestCase, mock
+from unittest.mock import patch
 
 import datetime
 
@@ -10,6 +11,16 @@ class EntryTests(unittest.TestCase):
     def test_get_date(self):
         answer1 = worklog.get_date()
         self.assertIsInstance(answer1, str)
+
+    def test_date_invalid(self):
+
+        to_pass_in = ['22/13/14', '12/13/14']
+
+        with patch('builtins.input', side_effect=to_pass_in):
+            with patch('builtins.print', side_effect=print) as mock:
+                result = worklog.get_date()
+                mock.assert_called_once_with("Opps! Wrong date format. Please post in MM/DD/YY format ")
+        
 
 if __name__ == '__main__':
     unittest.main()
