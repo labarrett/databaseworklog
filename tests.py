601112e27b3c2e30424ad780558d5c7ea659b706
Lindsay Barrett <labarrett22@gmail.com>
2019-04-23 21:25:18 -0700

    Added new tests
    Added new tests

--- tests.py
+++ tests.py
@@ -3,14 +3,27 @@ from unittest import TestCase, mock
 from unittest.mock import patch
 
 import datetime
-
+from peewee import SqliteDatabase
 import worklog
 
+MODELS = [worklog.Entry]
+
+test_db = SqliteDatabase(':memory:')
+
 class EntryTests(unittest.TestCase):
+    def setUp(self):
+          test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+          test_db.connect
+          test_db.create_tables(MODELS)
+  
 
     def test_get_date(self):
-        answer1 = worklog.get_date()
-        self.assertIsInstance(answer1, str)
+        to_pass_in = ['12/13/14']
+
+        with patch('builtins.input', side_effect=to_pass_in):
+            with patch('builtins.print', side_effect=print) as mock:
+                result = worklog.get_date()
+                self.assertIsInstance(result, str)
 
     def test_date_invalid(self):
 
@@ -21,6 +34,24 @@ class EntryTests(unittest.TestCase):
                 result = worklog.get_date()
                 mock.assert_called_once_with("Opps! Wrong date format. Please post in MM/DD/YY format ")
         
+       
+    def test_add_entries(self):
+    
+      to_pass_in = ['Lindsay', 'Task 1', '12/13/14', '00:45', 'no notes', 'Y', 'q']
+      
+      with patch('builtins.input', side_effect=to_pass_in):
+            result = worklog.add_entry()
+            self.assertIsNotNone(result)
 
+
+    def test_search_menu(self):
+    
+      to_pass_in = ['a','Lindsay', 'Task 1', '12/13/14', '00:45', 'no notes', 'Y', 'q']
+      
+      with patch('builtins.input', side_effect=to_pass_in):
+              result = worklog.menu_loop()
+              self.assertIsNotNone(result)
+              
+              
 if __name__ == '__main__':
     unittest.main()
